0be6876aeb7e26f262c5eb46d79580da756b619f
Neb <b.stacy1297@gmail.com>
2019-10-12 13:26:28 -0400

    experimented with labels, buttons, and images in tkinter

--- GUI.py
+++ GUI.py
@@ -1,13 +1,29 @@
 import tkinter
+import tkinter.messagebox
 
 class GUI:
     def __init__(self):
         self.main_window = tkinter.Tk()
-        self.label1 = tkinter.Label(self.main_window, text='Where is the ISS?')
-        self.label2 = tkinter.Label(self.main_window, text='Where is the ISS?')
-        self.label1.pack()
-        self.label2.pack()
+        self.background_image=tkinter.PhotoImage('~/WheresTheISS/img/map.gif')
+        self.background_label = tkinter.Label(image=self.background_image)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.findbutton = tkinter.Button(self.main_window, text='Find it!', command=self.do_something)
+        self.findbutton.pack()
+        self.quitbutton = tkinter.Button(self.main_window, text='Quit', command=self.main_window.destroy)
+        self.quitbutton.pack()
+        self.top_frame = tkinter.Frame(self.main_window)
+        self.bottom_frame = tkinter.Frame(self.main_window)
+        self.label1 = tkinter.Label(self.top_frame, text='Where is the ISS?')
+        self.label2 = tkinter.Label(self.bottom_frame, text='Where is the ISS?')
+        self.label1.pack(side='top')
+        self.label2.pack(side='left')
+        self.top_frame.pack()
+        self.bottom_frame.pack()
+        self.background_label.image = self.background_image
         tkinter.mainloop()
+
+    def do_something(self):
+        tkinter.messagebox.showinfo('Response', 'The ISS was found at: ')
 
 gui = GUI()
 
